cdff04b538437a96baa33811a4a7afc6c7f09c52
Matěj Bartoň <matej.8.barton@gmail.com>
2022-01-01 22:42:24 +0100

    Adds topNav menu

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.app');
-});
+// Route::get('/', function () {
+//     return view('layouts.app');
+// });
+
+Route::view('/', 'previews.homepage')->name('homepage');
+Route::view('/artwork', 'previews.artwork')->name('artwork');
+Route::view('/biography', 'previews.biography')->name('biography');
+Route::view('/news', 'previews.news')->name('news');
+Route::view('/contact', 'previews.contact')->name('contact');
+
