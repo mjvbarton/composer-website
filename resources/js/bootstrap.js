245818249eb764a9b86057c93ce635d3148556bc
Matěj Bartoň <matej.8.barton@gmail.com>
2022-01-02 20:40:13 +0100

    Installs fontawesome plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,8 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
+require('@fortawesome/fontawesome-free/js/all.js');
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
