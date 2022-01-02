34063dd40bced7c09b2eb199830cf8c563ff523a
Matěj Bartoň <matej.8.barton@gmail.com>
2022-01-02 20:44:22 +0100

    Adds preview for contact page

--- resources/views/previews/contact.blade.php
+++ resources/views/previews/contact.blade.php
@@ -17,8 +17,45 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         
     </head>
-    <body class="bg-orange-200">
-        <x-top-nav activeMenu="contact"></x-top-nav>
+    <body>
+        <x-top-nav activeMenu="contact" />
+
+        <main id="contact" class="min-h-screen">
+            <div class="relative top-40 w-min bg-white px-20 py-5 mx-auto">
+                <h1 class="text-4xl uppercase font-display tracking-widest text-pink-600">{{ __('Contact')}}</h1>
+            </div>
+
+            <section class="relative top-60 bg-white w-3/5 mx-auto p-10">
+                <h2 class="font-display font-thin text-3xl uppercase text-pink-600 mb-2">{{ __('Contact form') }}</h2>
+                <div class="prose min-w-full mb-10">
+                    <p>{{ __('If you want to contact me, please fill up this contact form. I will reply as soon as possible.') }}</p>
+                </div>
+
+                <form action="{{ request()->route()->uri() }}" method="post">
+                    <div class="flex flex-col space-y-7">
+                        <div class="flex flex-col">
+                            <label for="contactName" class="font-display uppercase text-pink-600 text-lg">{{ __('Your name') }}:</label>
+                            <input type="text" name="name" id="contactName" required placeholder="{{ __('e.g. Igor Stravinskij')}}">
+                            <small class="text-red-500 font-semibold">Please fill up this field!</small>
+                        </div>
+                        <div class="flex flex-col">
+                            <label for="contactEmail" class="font-display uppercase text-pink-600 text-lg">{{ __('Your email') }}:</label>
+                            <input type="email" name="email" id="contactEmail" required placeholder="{{ __('e.g. igor@stravinskij.com')}}">
+                            <small class="text-red-500 font-semibold">Please fill up this field!</small>
+                        </div> 
+                        <div class="flex flex-col">
+                            <label for="contactEmail" class="font-display uppercase text-pink-600 text-lg">{{ __('Your message') }}:</label>
+                            <textarea name="message" id="contactMessage" required placeholder="{{ __('e.g. Hello there!') }}"></textarea>
+                            <small class="text-red-500 font-semibold">Please fill up this field!</small>
+                        </div>                  
+                        <div class="flex flex-col">                            
+                            <input type="checkbox" id="gdprConsent" name="gdprConsent" required value="I consent">
+                            <small class="text-red-500 font-semibold">Please fill up this field!</small>
+                        </div>
+                    </div>
+                </form>
+            </section>
+        </main>
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}"></script>
