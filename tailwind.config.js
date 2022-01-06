28262941e32fd6002385617b7f56aff84fdbcb9e
Matěj Bartoň <matej.8.barton@gmail.com>
2022-01-06 21:17:32 +0100

    Removes @tailwindcss/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,7 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),    
     require('@tailwindcss/aspect-ratio')
   ],
 }
