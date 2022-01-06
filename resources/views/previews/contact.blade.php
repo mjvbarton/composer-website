6826785d56ddd888441b9b67478745f6c6f37c19
Matěj Bartoň <matej.8.barton@gmail.com>
2022-01-06 21:18:17 +0100

    Updates contact form

--- resources/views/previews/contact.blade.php
+++ resources/views/previews/contact.blade.php
@@ -17,46 +17,64 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         
     </head>
-    <body>
+    <body class="selection:bg-pink-200">
         <x-top-nav activeMenu="contact" />
 
-        <main id="contact" class="min-h-screen">
+        <main id="contact" class="min-h-screen h-full">
             <div class="relative top-40 w-min bg-white px-20 py-5 mx-auto">
                 <h1 class="text-4xl uppercase font-display tracking-widest text-pink-600">{{ __('Contact')}}</h1>
             </div>
 
-            <section class="relative top-60 bg-white w-3/5 mx-auto p-10">
+            <section class="mt-60 bg-white w-3/5 mx-auto p-10">
                 <h2 class="font-display font-thin text-3xl uppercase text-pink-600 mb-2">{{ __('Contact form') }}</h2>
                 <div class="prose min-w-full mb-10">
                     <p>{{ __('If you want to contact me, please fill up this contact form. I will reply as soon as possible.') }}</p>
                 </div>
 
-                <form action="{{ request()->route()->uri() }}" method="post">
-                    <div class="flex flex-col space-y-7">
-                        <div class="flex flex-col">
-                            <label for="contactName" class="font-display uppercase text-pink-600 text-lg">{{ __('Your name') }}:</label>
-                            <input type="text" name="name" id="contactName" required placeholder="{{ __('e.g. Igor Stravinskij')}}">
-                            <small class="text-red-500 font-semibold">Please fill up this field!</small>
-                        </div>
-                        <div class="flex flex-col">
-                            <label for="contactEmail" class="font-display uppercase text-pink-600 text-lg">{{ __('Your email') }}:</label>
-                            <input type="email" name="email" id="contactEmail" required placeholder="{{ __('e.g. igor@stravinskij.com')}}">
-                            <small class="text-red-500 font-semibold">Please fill up this field!</small>
-                        </div> 
-                        <div class="flex flex-col">
-                            <label for="contactEmail" class="font-display uppercase text-pink-600 text-lg">{{ __('Your message') }}:</label>
-                            <textarea name="message" id="contactMessage" required placeholder="{{ __('e.g. Hello there!') }}"></textarea>
-                            <small class="text-red-500 font-semibold">Please fill up this field!</small>
-                        </div>                  
-                        <div class="flex flex-col">                            
-                            <input type="checkbox" id="gdprConsent" name="gdprConsent" required value="I consent">
-                            <small class="text-red-500 font-semibold">Please fill up this field!</small>
-                        </div>
-                    </div>
+                <form action="{{ request()->route()->uri() }}" method="post" class="flex flex-col space-y-7">                    
+                    <!-- Name -->
+                    <label class="flex flex-col space-y-2">
+                        <span class="font-display uppercase text-pink-600 text-lg peer-required:after:content-['*']">{{ __('Your name') }}:</span>
+                        <input type="text" name="name" required minlength="3" maxlength="300" placeholder="{{ __('e.g. Igor Stravinskij')}}" class="peer border p-2 focus:outline focus:outline-1 focus:outline-pink-600 hover:bg-gray-50 transition-all ease-in duration-200">
+                        <small class="invisible peer-invalid:visible text-red-500 font-semibold">Please enter your name</small>
+                    </label>
+
+                    <!-- Email -->
+                    <label class="flex flex-col space-y-2">
+                        <span class="font-display uppercase text-pink-600 text-lg">{{ __('Your email') }}:</span>
+                        <input type="email" name="email" required minlength="3" maxlength="300" placeholder="{{ __('e.g. igor@stravinskij.com')}}" class="peer border p-2 focus:outline focus:outline-1 focus:outline-pink-600 hover:bg-gray-50 transition-all ease-in duration-200">
+                        <small class="invisible peer-invalid:visible text-red-500 font-semibold">Please enter valid email address</small>
+                    </label>
+
+                    <!-- Message -->
+                    <label class="flex flex-col space-y-2">
+                        <span class="font-display uppercase text-pink-600 text-lg">{{ __('Your message') }}:</span>
+                        <textarea name="message" required minlength="3" maxlength="1000" placeholder="{{ __('e.g. Some nice message!')}}" class="peer border p-2 focus:outline focus:outline-1 focus:outline-pink-600 hover:bg-gray-50 transition-all ease-in duration-200"></textarea>
+                        <small class="invisible peer-invalid:visible text-red-500 font-semibold">Please enter some message</small>
+                    </label> 
+                    
+                    <!-- GDPR Consent -->                
+                    <label class="flex space-x-2 items-center">
+                        <span class="sr-only">{{ __('GDPR Consent') }}:</span>                                                    
+                            <input type="checkbox" name="gdprConsent" required class="peer border p-2 focus:outline focus:outline-1 focus:outline-pink-600 hover:bg-gray-50 c transition-all ease-in duration-200">
+                        <span class="prose min-w-fit">
+                            I consent with the <a href="{{ URL::route('privacy-policy') }}" target="__blank">Privacy policy</a>.
+                        </span>
+                        <small class="invisible peer-invalid:visible text-red-500 font-semibold transition-all ease-in duration-200">You must consent with the gdpr policy in order to get your message processed.</small>
+                    </label>
+                    
+                    <button class="block place-self-start p-2 text-white text-center font-display tracking-wide uppercase bg-pink-600 hover:bg-pink-700 focus:outline-none focus:bg-pink-700 active:bg-pink-800 shadow-lg transition-all ease-in duration-200">
+                        <span class="mr-1">
+                            <i class="far fa-paper-plane"></i>
+                        </span>
+                        Odeslat
+                    </button>
                 </form>
             </section>
         </main>
 
+        <x-footer />
+        
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
