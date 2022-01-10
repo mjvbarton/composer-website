75985c6e1c0c1de41a1bfc258f696e266e9d9fd1
Matěj Bartoň <matej.8.barton@gmail.com>
2022-01-10 17:59:17 +0100

    Initial commit for wordpress theme

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
+
+mix.sass('sass/style.scss', 'style.css')
+.options({
+    processCssUrls: false,
+    postCss: [tailwindcss('tailwind.config.js')]
+});
+
+mix.browserSync(process.env.MIX_BROWSERSYNC);
